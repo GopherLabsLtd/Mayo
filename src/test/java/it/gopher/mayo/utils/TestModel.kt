97353039e61533eefdff0260ca5899284c9d193c
Alex Nguyen <alexnguyen@live.ca>
2017-11-30 22:28:37 -0500

    added more modules

--- src/test/java/it/gopher/mayo/utils/TestModel.kt
+++
@@ -1,9 +0,0 @@
-package it.gopher.mayo.utils
-
-import com.google.gson.annotations.SerializedName
-
-/**
- * Generic Model for use in testing.
- */
-data class TestModel(@SerializedName("test")
-                     val test: String)
